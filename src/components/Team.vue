b4c956c3cd5135ba3573f20b7d329330e9822248
hvekriya <haresh.1994@hotmail.com>
2019-06-24 15:20:07 +0100

    Bug fix on slice section heading

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
     <!-- Team Members -->
-    <h2>Our Team</h2>
-    <div class="row">
+
       <template v-for="(slice, index) in fields.slices">
         <template v-if="slice.slice_type === 'team'">
-          <div class="col-xl-3 col-md-6 mb-4" v-for="(item, index) in slice.items" :key="'photo-' + index">
-            <div class="card border-0 shadow">
-              <prismic-image :field="item.portrait" class="card-img-top" alt="item.portrait[index].alt" />
-              <div class="card-body text-center">
-                <prismic-rich-text :field="item.first_and_lastname" class="card-title mb-0" />
-                <prismic-rich-text :field="item.position" class="card-text text-black-50" />
+          <prismic-rich-text :field="slice.primary.team_section"/>
+          <div class="row">
+            <div class="col-xl-3 col-md-6 mb-4" v-for="(item, index) in slice.items" :key="'photo-' + index">
+              <div class="card border-0 shadow">
+                <prismic-image :field="item.portrait" class="card-img-top" alt="item.portrait[index].alt" />
+                <div class="card-body text-center">
+                  <prismic-rich-text :field="item.first_and_lastname" class="card-title mb-0" />
+                  <prismic-rich-text :field="item.position" class="card-text text-black-50" />
+                </div>
               </div>
             </div>
           </div>
         </template>
       </template>
-    </div>
   </div>
 </template>
 
