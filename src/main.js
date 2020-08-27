18c2111f03aa808985b53f273fa70e9c0b07debc
Haresh Vekriya <haresh.vekriya@gmail.com>
2020-08-27 17:50:13 +0100

    updated the sitemap

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import * as VueGoogleMaps from "vue2-google-maps";
 import router from "./router";
 import "./registerServiceWorker";
 
+
 const accessToken = process.env.VUE_APP_PRISMIC;
 
 Vue.config.productionTip = false;
@@ -28,13 +29,13 @@ Vue.use(VueGoogleMaps, {
   }
 });
 
-Vue.filter("formatDate", function(value) {
+Vue.filter("formatDate", function (value) {
   if (value) {
     return moment(String(value)).format("DD/MM/YYYY");
   }
 });
 
-Vue.filter("readMore", function(text, length, suffix) {
+Vue.filter("readMore", function (text, length, suffix) {
   let newText = text
     .map(a => a.text)
     .filter(b => b)
@@ -42,7 +43,7 @@ Vue.filter("readMore", function(text, length, suffix) {
   return newText.substring(0, length) + suffix;
 });
 
-Vue.filter("truncate", function(value) {
+Vue.filter("truncate", function (value) {
   if (value.length > 20) {
     value = value.substring(0, 17) + "...";
   }
@@ -52,22 +53,17 @@ Vue.filter("truncate", function(value) {
 Vue.mixin({
   methods: {
     randomCover: () => {
-      let coverList = [
-        {
-          url:
-            "https://balkrishna.cdn.prismic.io/balkrishna/6896f63e22fa9cee3f7f08d191532933c8186937_abc.jpg"
+      let coverList = [{
+          url: "https://balkrishna.cdn.prismic.io/balkrishna/6896f63e22fa9cee3f7f08d191532933c8186937_abc.jpg"
         },
         {
-          url:
-            "https://balkrishna.cdn.prismic.io/balkrishna/d7678442e32d849198e6457f01f9556c5510d654_legojpg.jpg"
+          url: "https://balkrishna.cdn.prismic.io/balkrishna/d7678442e32d849198e6457f01f9556c5510d654_legojpg.jpg"
         },
         {
-          url:
-            "https://balkrishna.cdn.prismic.io/balkrishna/5536b5de1e1dd00784c8b7a6eb6644192bee5643_pencil.jpg"
+          url: "https://balkrishna.cdn.prismic.io/balkrishna/5536b5de1e1dd00784c8b7a6eb6644192bee5643_pencil.jpg"
         },
         {
-          url:
-            "https://balkrishna.cdn.prismic.io/balkrishna/ddb2a7afc3d6e8220b1a8854f70bb845a2f74231_colored-pencils.jpg"
+          url: "https://balkrishna.cdn.prismic.io/balkrishna/ddb2a7afc3d6e8220b1a8854f70bb845a2f74231_colored-pencils.jpg"
         }
       ];
       let random = Math.floor(Math.random() * coverList.length);
